b07ab96ff392f8333456e1071c97c8777f048e0b
Karol Błażejewski <carolinii.contact@gmail.com>
2022-12-20 18:08:56 +0000

    clean the code

--- app_functions.py
+++ app_functions.py
@@ -4,12 +4,10 @@ import constants
 
 
 def run_app() -> None:
-    print('----------[ wczytujemy dane ]----------')
-    start_height = read_data('Podaj wysokość początkową (w m): ', 10)
-    start_velocity = read_data('Podaj prędkość początkową (w m/s): ', 2)
-    print(f'{start_height}, {start_velocity}')
-
-    print('OK, dane początkowe wczytane, działamy dalej.')
+    print(constants.READING_DATA_PROMPT.center(constants.PROMPT_WIDTH, constants.SIGN_TO_FILL_WIDTH))
+    start_height = read_data(constants.READ_HEIGHT_PROMPT, constants.HEIGHT_RANGE)
+    start_velocity = read_data(constants.READ_VELOCITY_PROMPT, constants.VELOCITY_RANGE)
+    print(constants.FINISH_READING_DATA_PROMPT.center(constants.PROMPT_WIDTH, constants.SIGN_TO_FILL_WIDTH))
 
     total_time = calculate_time_to_get_ground(start_height)
     max_range = calculate_range(start_velocity, total_time)
@@ -20,10 +18,10 @@ def read_data(user_prompt: str, min_value: int) -> float:
         try:
             result = float(input(user_prompt))
         except ValueError:
-            print('To nie jest liczba zmiennoprzecinkowa (np. 3.14).')
+            print(constants.VALUE_ERROR_MESSAGE)
         else:
             if result < min_value:
-                print(f'Minimalna wartość powinna wynosić: {min_value}')
+                print(constants.RANGE_ERROR_MESSAGE + str(min_value))
                 continue
             break
 
--- constants.py
+++ constants.py
@@ -1,2 +1,15 @@
+READING_DATA_PROMPT = '[ Wczytywanie danych ]'
+FINISH_READING_DATA_PROMPT = '[ Dane wczytane ]'
+PROMPT_WIDTH = 30
+SIGN_TO_FILL_WIDTH = '-'
+
+READ_HEIGHT_PROMPT = 'Podaj wysokość początkową (w m): '
+HEIGHT_RANGE = 10
+READ_VELOCITY_PROMPT = 'Podaj prędkość początkową (w m/s): '
+VELOCITY_RANGE = 2
+
+VALUE_ERROR_MESSAGE = 'To nie jest liczba zmiennoprzecinkowa (np. 3.14).'
+RANGE_ERROR_MESSAGE = 'Minimalna wartość powinna wynosić: '
+
 G = 9.81
 NUMBER_TWO = 2
